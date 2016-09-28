cab20eb58883c9fdf181bebb57dc93d75b407d6a
Robert Dumitrescu <robertdum01@gmail.com>
2016-09-28 19:06:36 +0100

    Added services

--- udemy-course/recipe-book/src/app/recipes/recipe.service.ts
+++ udemy-course/recipe-book/src/app/recipes/recipe.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@angular/core';
 import {Recipe} from "./recipe";
+import {Ingredient} from "../shared/ingredient";
 @Injectable()
 export class RecipeService {
 
   private recipes: Recipe[] = [
-    new Recipe('Schnitzel', 'Very tasty', 'http://images.derberater.de/files/imagecache/456xXXX_berater/berater/slides/WienerSchnitzel.jpg', []),
+    new Recipe('Schnitzel', 'Very tasty', 'http://images.derberater.de/files/imagecache/456xXXX_berater/berater/slides/WienerSchnitzel.jpg', [
+        new Ingredient("French fries", 2),
+        new Ingredient("Pork Meat", 1)
+    ]),
     new Recipe('Summer Salad', 'Okayish', 'http://ohmyveggies.com/wp-content/uploads/2013/06/the_perfect_summer_salad.jpg', [])
   ];
 
---
+++ udemy-course/recipe-book/src/app/shopping-list/shopping-list.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {Ingredient} from "../shared/ingredient";
+
+@Injectable()
+export class ShoppingListService {
+
+  private items: Ingredient[] = [];
+
+  getItems() {
+    return this.items;
+  }
+
+  addItems(items: Ingredient[]) {
+    Array.prototype.push.apply(this.items, items);
+  }
+
+  constructor() { }
+
+}
